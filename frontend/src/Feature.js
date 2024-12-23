ccd15a87b61a378d0a98aa5673e2ebd74112a8c2
mryunt02 <mrthresh58@gmail.com>
2024-12-23 15:41:30 +0300

    better structure

---
+++ frontend/src/Feature.js
@@ -0,0 +1,62 @@
+import {
+  FaUserFriends,
+  FaRegHeart,
+  FaClipboardList,
+  FaRunning,
+  FaWeight,
+  FaCalendarAlt,
+  FaAppleAlt,
+  FaDumbbell,
+  FaChartLine,
+} from 'react-icons/fa';
+
+const features = [
+  {
+    icon: <FaDumbbell />,
+    title: 'Track Your Workouts',
+    description: 'Log your workouts and monitor your progress over time.',
+  },
+  {
+    icon: <FaAppleAlt />,
+    title: 'Meal Planning',
+    description: 'Plan your meals and maintain a balanced diet effortlessly.',
+  },
+  {
+    icon: <FaClipboardList />,
+    title: 'Workout Plans',
+    description: 'Access tailored workout plans to meet your fitness goals.',
+  },
+  {
+    icon: <FaChartLine />,
+    title: 'Monitor Your Progress',
+    description: 'Keep track of your fitness journey and achieve your goals.',
+  },
+  {
+    icon: <FaUserFriends />,
+    title: 'Join a Community',
+    description: 'Connect with friends and share your fitness journey.',
+  },
+  {
+    icon: <FaRegHeart />,
+    title: 'Health Insights',
+    description: 'Get personalized health insights based on your data.',
+  },
+  {
+    icon: <FaClipboardList />,
+    title: 'Workout Plans',
+    description: 'Access tailored workout plans to meet your fitness goals.',
+  },
+  {
+    icon: <FaRunning />,
+    title: 'Running Tracker',
+    description: 'Track your running sessions and analyze your performance.',
+  },
+  {
+    icon: <FaWeight />,
+    title: 'Weight Management',
+    description:
+      'Monitor your weight and set goals to achieve a healthy lifestyle.',
+  },
+];
+
+export { features };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,6 +8,7 @@ import {
   FaClipboardList,
 } from 'react-icons/fa'; // Import icons
 import FeatureCard from '../components/FeatureCard';
+import { features } from '../Feature';
 
 const Home = () => {
   return (
@@ -26,36 +27,14 @@ const Home = () => {
       </header>
 
       <section className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-        <FeatureCard
-          icon={<FaDumbbell />}
-          title='Track Your Workouts'
-          description='Log your workouts and monitor your progress over time.'
-        />
-        <FeatureCard
-          icon={<FaAppleAlt />}
-          title='Meal Planning'
-          description='Plan your meals and maintain a balanced diet effortlessly.'
-        />
-        <FeatureCard
-          icon={<FaChartLine />}
-          title='Monitor Your Progress'
-          description='Keep track of your fitness journey and achieve your goals.'
-        />
-        <FeatureCard
-          icon={<FaUserFriends />}
-          title='Join a Community'
-          description='Connect with friends and share your fitness journey.'
-        />
-        <FeatureCard
-          icon={<FaRegHeart />}
-          title='Health Insights'
-          description='Get personalized health insights based on your data.'
-        />
-        <FeatureCard
-          icon={<FaClipboardList />}
-          title='Workout Plans'
-          description='Access tailored workout plans to meet your fitness goals.'
-        />
+        {features.map((feature) => (
+          <FeatureCard
+            key={feature.title}
+            icon={feature.icon}
+            title={feature.title}
+            description={feature.description}
+          />
+        ))}
       </section>
       <footer className='mt-12 text-center'>
         <p className='text-gray-600'>
