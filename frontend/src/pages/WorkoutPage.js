6ca334d4c5dc9b15165d0546e641c29e3b31e4fd
mryunt02 <mrthresh58@gmail.com>
2024-12-24 20:50:35 +0300

    add workout

--- frontend/src/pages/WorkoutPage.js
+++ frontend/src/pages/WorkoutPage.js
@@ -38,7 +38,6 @@ const WorkoutPage = () => {
 
     fetchWorkouts();
   }, []);
-  console.log(workouts);
 
   const validateInputs = () => {
     const newErrors = {};
@@ -67,15 +66,33 @@ const WorkoutPage = () => {
     return Object.keys(newErrors).length === 0;
   };
 
-  const handleAddWorkout = (e) => {
+  const handleAddWorkout = async (e) => {
     e.preventDefault();
     if (validateInputs()) {
-      setWorkouts([...workouts, { ...newWorkout, id: Date.now() }]);
-      setNewWorkout({
-        date: new Date().toISOString().split('T')[0],
-        exercises: [{ name: '', reps: '', sets: '', duration: '' }],
-      });
-      setErrors({});
+      try {
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+        const response = await axiosInstance.post(
+          '/api/workouts',
+          {
+            ...newWorkout,
+            userId,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setWorkouts([...workouts, response.data]);
+        setNewWorkout({
+          date: new Date().toISOString().split('T')[0],
+          exercises: [{ name: '', reps: '', sets: '', duration: '' }],
+        });
+        setErrors({});
+      } catch (error) {
+        console.error('Error adding workout:', error);
+      }
     }
   };
 
